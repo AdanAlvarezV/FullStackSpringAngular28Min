d2d858c7be504091ae5f0bd10dcaf80c9fbbc2bc
AdanAlvarezV <ing.adanalvarez.valenzuela@gmail.com>
2023-02-07 12:01:08 -0600

    Dockerfile

--- back/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/model/Todo.java
+++ back/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/model/Todo.java
@@ -4,17 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@Entity
-public class Todo {
+@Entity
+public class Todo implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String username;
     private String description;
     private Date targetDate;
